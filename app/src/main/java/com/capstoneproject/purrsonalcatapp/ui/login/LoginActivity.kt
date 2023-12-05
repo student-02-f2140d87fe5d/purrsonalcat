9d0a922f1cbc17fb844c87452c89534efa3a158e
ihsankarim <ihsanalkarim303@gmail.com>
2023-12-05 19:49:16 +0700

    initial commit

---
+++ app/src/main/java/com/capstoneproject/purrsonalcatapp/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.purrsonalcatapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.purrsonalcatapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/capstoneproject/purrsonalcatapp/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.purrsonalcatapp.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.purrsonalcatapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
