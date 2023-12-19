96a9106c8dead46730f3aeeaf5e6b93d43812085
malik <malilfajar17@gmail.com>
2023-12-20 05:15:21 +0700

    Add authMiddleware to predict route

--- src/routes/predict.js
+++ src/routes/predict.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const { getQuestions, processQuestion } = require('../controllers/predicts');
 
+router.use(require('../middleware/authMiddleware'));
+
 router.get('/', getQuestions);
 router.post('/', processQuestion);
 
